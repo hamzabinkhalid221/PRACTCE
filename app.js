3a6b9d065a41503abbd8b735922036fd5c757ea4
hamzabinkhalid221 <142832021+hamzabinkhalid221@users.noreply.github.com>
2023-10-06 19:18:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+                  
+
+// var table  = prompt("Enter the table");
+// var length = prompt("Enter the length");
+// for(var a=1; a<=length; a++){
+//     document.write(table + " * " +a+ " = " +(a*table) +"<br>");
+// }
+ 
+                    // WHILE  
+// var table  = prompt("Enter the table");
+// var length = prompt("Enter the length");
+// var a=1;
+// while(a<=length)
+// {
+//   document.write(table + " * " +a +" = " +(a*table) +"<br>");
+//  a++;
+// }
+
+
+//             //   DO WHILE
+// var table  = prompt("Enter the table");
+// var length = prompt("Enter the length");
+// var a=1;
+// do{
+//     document.write(table + " * " +a+ " =" +(a*table) +"<br>");
+//     a++;
+// }while (a<=length);
