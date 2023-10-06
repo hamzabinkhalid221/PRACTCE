46eb63f71b9e9192dd9dca33543b9ded141a4162
hamzabinkhalid221 <142832021+hamzabinkhalid221@users.noreply.github.com>
2023-10-06 21:14:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -25,3 +25,24 @@
 //     document.write(table + " * " +a+ " =" +(a*table) +"<br>");
 //     a++;
 // }while (a<=length);
+
+
+
+    <input id="inp" type="text" placeholder="Enter the table"><br>
+    <input id="is"type="text" placeholder="Enter length"><br>
+    <button onclick="set()">set</button>
+
+
+function set(){
+
+
+var inp = document.getElementById("inp");
+var is = document.getElementById("is");
+var table = inp.value;
+var length = is.value;
+for(var a=1;a<=length;a++){
+    document.write(table + " * " +a+ " = " +(a*table) +"<br>");
+
+}
+}
+
